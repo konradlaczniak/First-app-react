5d556a46f6e273bb8ee481fcfa9009092c2fb5a6
konradlaczniak <konrad.laczniak@gmail.com>
2020-04-26 16:29:08 +0200

    Validation added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    email: "",
+    password: "",
+    isSubmitted: false,
+    isEmail: false,
+    isPassword: false
+  };
+
+  handleLogin = (event) => {
+
+    if(this.state.email === "" || this.state.password === "") {
+      if (this.state.email === "")
+        this.setState({ isEmail: true});
+      else {
+        this.setState({ isEmail: false});
+        }
+      if (this.state.password === "")
+        this.setState({isPassword: true});
+      else {
+        this.setState({isPassword: false});
+      }
+    }
+    else{
+      this.setState({ isSubmitted: true });
+    }
+  };
+
+  handleChangeEmail = (event) => {
+    this.setState({ email: event.target.value });
+  };
+
+  handleChangePassword = (event) => {
+    this.setState({ password: event.target.value });
+  };
+
+  handleLogout = () => {
+    this.setState({ password: "", email: "", isSubmitted: false });
+  };
+
+  render() {
+    return (
+      <>
+        {this.state.isSubmitted === false && (
+          <section>
+            <div className="inputRow">
+              <label>E-mail: </label>
+              <input
+                onChange={this.handleChangeEmail}
+                value={this.state.email}
+              />
+              {this.state.isEmail === true && (
+                <div className="validation">
+                  <p>Please fill in your E-mail</p>
+                </div>
+              )}
+            </div>
+
+            <div className="inputRow">
+              <label>Password: </label>
+              <input
+                onChange={this.handleChangePassword}
+                type="password"
+                value={this.state.password}
+              />
+              {this.state.isPassword === true && (
+                <div className="validation">
+                  <p>Please fill in your password</p>
+                </div>
+              )}
+            </div>
+
+            <button onClick={this.handleLogin}>Login</button>
+          </section>
+        )}
+
+        {this.state.isSubmitted === true && (
+          <section>
+            <div>
+              <label>E-mail:</label> {this.state.email}
+            </div>
+
+            <div>
+              <label>Password:</label> {this.state.password}
+            </div>
+            <button onClick={this.handleLogout}>Logout</button>
+          </section>
+        )}
+      </>
+    );
+  }
 }
 
 export default App;
